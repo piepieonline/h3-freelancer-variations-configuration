4068117cb20d13399f8c7bf1577b9b4e1a754811
Piepieonline <piepieonline@gmail.com>
2022-12-11 15:21:44 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+(async () => {
+    const { missions } = await import("/missions.js");
+    const { guidToName, brickToName } = await import("/friendlyNames.js");
+
+    const selectedMissions = {};
+
+    const radioParent = document.getElementById('radio-parent');
+    radioParent.innerHTML = '';
+
+    let htmlToAdd = `<div class="accordion" id="mission-accordion">`;
+    Object.keys(missions).forEach((mission, missionIndex) => {
+        // radioParent.insertAdjacentHTML('beforeend', `<span>${mission}</span>`);
+
+        htmlToAdd += `<div id=${mission} class="accordion-item">
+                        <div class="accordion-header" id="heading${missionIndex}">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${missionIndex}" aria-expanded="true" aria-controls="collapse${missionIndex}">${guidToName[mission]}&nbsp;<span id="${mission}-selected-counter">${missions[mission].length}</span>/${missions[mission].length}&nbsp;&nbsp;&nbsp;<span style="color: grey">${mission}</span></button>
+                        </div>`;
+
+        selectedMissions[mission] = {};
+
+        missions[mission].forEach((brick, brickIndex) => {
+            const isEnabled = true;
+
+            htmlToAdd +=
+                `<div id="collapse${missionIndex}" class="accordion-collapse collapse" aria-labelledby="heading${missionIndex}">
+                    <div class="accordion-body">
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="${mission}-${brick}" ${isEnabled ? 'checked' : ''} onchange="modifySelection('${mission}', '${brick}')">
+                            <label class="form-check-label" for="flexSwitchCheckChecked">${brickToName[brick]} <span style="color: grey">(${brick})</span></label>
+                        </div>
+                    </div>
+                </div>`;
+
+            selectedMissions[mission][brick] = isEnabled;
+        });
+
+        htmlToAdd += `</div>`;
+    });
+    htmlToAdd += `</div>`;
+
+    radioParent.insertAdjacentHTML('beforeend', htmlToAdd);
+
+    window.modifySelection = (mission, brick) => {
+        console.log(document.getElementById(`${mission}-${brick}`).checked);
+        selectedMissions[mission][brick] = document.getElementById(`${mission}-${brick}`).checked;
+        document.getElementById(`${mission}-selected-counter`).innerText = Object.values(selectedMissions[mission]).filter(val => val).length;
+    };
+
+    window.generate = () => {
+        var missionJson = {};
+        Object.keys(selectedMissions).forEach(mission => {
+            missionJson[mission] = [];
+            Object.keys(selectedMissions[mission]).forEach(brick => {
+                if(selectedMissions[mission][brick])
+                {
+                    missionJson[mission].push(brick);
+                }
+            });
+        });
+
+        downloadObjectAsJson(missionJson, 'FreelancerVariationMissions');
+    };
+
+    function downloadObjectAsJson(exportObj, exportName){
+        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj, null, 2));
+        var downloadAnchorNode = document.createElement('a');
+        downloadAnchorNode.setAttribute("href",     dataStr);
+        downloadAnchorNode.setAttribute("download", exportName + ".json");
+        document.body.appendChild(downloadAnchorNode); // required for firefox
+        downloadAnchorNode.click();
+        downloadAnchorNode.remove();
+    }
+})();
---
+++ missions.js
@@ -0,0 +1,149 @@
+export const missions = {
+    "060a48f0-117b-4b22-a165-111fd4a5b745": [
+        "assembly:/_pro/scenes/missions/rocky/tod_midday.brick",
+        "assembly:/_pro/scenes/missions/rocky/tod_sunset.brick",
+        "assembly:/_pro/scenes/missions/rocky/override_poker.brick"
+    ],
+    "76fd7c40-83a0-48c9-be49-cef107583eae": [
+        "assembly:/_pro/scenes/missions/hokkaido/mission_mild_flu.brick",
+        "assembly:/_pro/scenes/missions/hokkaido/mission_mild_mamushi.brick",
+        "assembly:/_pro/scenes/missions/hokkaido/tod_flu.brick",
+        "assembly:/_pro/scenes/missions/hokkaido/override_shogi.brick"
+    ],
+    "ebb8cdab-e521-4ba8-9288-d697b1aaaba7": [
+        "assembly:/_pro/scenes/missions/paris/location_parisnoel.brick",
+        "assembly:/_pro/scenes/missions/paris/gamechangers_wild_card_juniper.brick",
+        "assembly:/_pro/scenes/missions/paris/tod_midday.brick",
+        "assembly:/_pro/scenes/missions/paris/tod_ets_mild.brick"
+    ],
+    "d40076af-74d1-484e-8936-1367d5b107ce": [
+        "assembly:/_pro/scenes/missions/bangkok/mission_mild_zika.brick",
+        "assembly:/_pro/scenes/missions/bangkok/gamechanger_wild_card_geranium.brick",
+        "assembly:/_pro/scenes/missions/bangkok/tod_zika_night.brick",
+        "assembly:/_pro/scenes/missions/bangkok/tod_warlord_midday.brick"
+    ],
+    "107100ca-f910-49a9-ac13-59d0bb5691a4": [
+        "assembly:/_pro/scenes/missions/colombia/mission_mild_anaconda.brick",
+        "assembly:/_pro/scenes/missions/colombia/gamechanger_hippo_zinnia.brick",
+        "assembly:/_pro/scenes/missions/colombia/gamechanger_hippo_holly.brick"
+    ],
+    "1a98c48e-d618-4d4d-b17b-90ca3b180d2a": [
+        "assembly:/_pro/scenes/missions/colorado_2/mission_mild_rabies.brick",
+        "assembly:/_pro/scenes/missions/colorado_2/gamechanger_wild_card_daisy.brick",
+        "assembly:/_pro/scenes/missions/colorado_2/lighting_day.brick"
+    ],
+    "d4719601-83ce-4ca9-83cd-5164331d643f": [
+        "assembly:/_pro/scenes/missions/coastaltown/tod_mild_sunset.brick",
+        "assembly:/_pro/scenes/missions/coastaltown/tod_ebola_night.brick",
+        "assembly:/_pro/scenes/missions/coastaltown/gamechanger_wild card_jasmine.brick"
+    ],
+    "d84fb174-2cbb-4337-8a66-f13f9fb170cd": [
+        "assembly:/_pro/scenes/missions/ancestral/tod_midday.brick",
+        "assembly:/_pro/scenes/missions/ancestral/override_bridge.brick",
+        "assembly:/_pro/scenes/missions/ancestral/mission_mild_dgs.brick"
+    ],
+    "0ffe44ea-d241-49ec-835c-74d5714db343": [
+        "assembly:/_pro/scenes/missions/edgy/mission_fox/tod_et_midday.brick",
+        "assembly:/_pro/scenes/missions/edgy/mission_fox/tod_sunset.brick",
+        "assembly:/_pro/scenes/missions/edgy/mission_fox/override_meyer.brick"
+    ],
+    "f075fbc3-79ef-4e42-9326-9572f712a298": [
+        "assembly:/_pro/scenes/missions/opulent/mission_stingray/tod_night.brick",
+        "assembly:/_pro/scenes/missions/opulent/mission_stingray/override_hilo.brick"
+    ],
+    "59c15d1e-c65e-4394-9906-46cdeb944c64": [
+        "assembly:/_pro/scenes/missions/mumbai/tod_night.brick",
+        "assembly:/_pro/scenes/missions/mumbai/tod_midday.brick",
+        "assembly:/_pro/scenes/missions/mumbai/gamechanger_monkshood_holyman_override.brick"
+    ],
+    "4d9b38ab-5ea8-4bc3-8fbe-273a36c72731": [
+        "assembly:/_pro/scenes/missions/wet/tod_redsnapper_cloudy.brick",
+        "assembly:/_pro/scenes/missions/wet/tod_midday.brick",
+        "assembly:/_pro/scenes/missions/wet/mission_mild_mahjong.brick"
+    ],
+    "dac2d6fd-4f25-4a63-b338-aef458c05a7f": [
+        "assembly:/_pro/scenes/missions/skunk/tod_elusive_night.brick",
+        "assembly:/_pro/scenes/missions/skunk/override_solitaire.brick"
+    ],
+    "4a34f12a-ab98-4dc5-9262-c600193b2629": [
+        "assembly:/_pro/scenes/missions/miami/tod_night_ass.brick",
+        "assembly:/_pro/scenes/missions/miami/override_pinochle.brick"
+    ],
+    "37be638c-4265-4622-9326-8835b8535180": [
+        "assembly:/_pro/scenes/missions/theark/tod_day_ets_mild.brick",
+        "assembly:/_pro/scenes/missions/theark/gamechanger_magpie_locked_jail_door.brick"
+    ],
+    "3e72588a-46ea-4cb8-a10f-e2242ca8cc99": [
+        "assembly:/_pro/scenes/missions/rocky/tod_midday.brick",
+        "assembly:/_pro/scenes/missions/rocky/tod_sunset.brick",
+        "assembly:/_pro/scenes/missions/rocky/override_poker.brick"
+    ],
+    "65659b19-fb1a-4a14-b074-65227ffac128": [
+        "assembly:/_pro/scenes/missions/hokkaido/mission_hot_flu.brick",
+        "assembly:/_pro/scenes/missions/hokkaido/mission_mild_mamushi.brick",
+        "assembly:/_pro/scenes/missions/hokkaido/tod_flu.brick",
+        "assembly:/_pro/scenes/missions/hokkaido/override_shogi.brick"
+    ],
+    "c945b720-12c4-4f62-8026-4b4011a67dff": [
+        "assembly:/_pro/scenes/missions/paris/location_parisnoel.brick",
+        "assembly:/_pro/scenes/missions/paris/gamechangers_wild_card_juniper.brick",
+        "assembly:/_pro/scenes/missions/paris/tod_midday.brick",
+        "assembly:/_pro/scenes/missions/paris/tod_ets.brick"
+    ],
+    "2f562dba-2b14-42f3-b9fb-eb72e79450ba": [
+        "assembly:/_pro/scenes/missions/bangkok/override_constantjeff.brick",
+        "assembly:/_pro/scenes/missions/bangkok/gamechanger_wild_card_geranium.brick",
+        "assembly:/_pro/scenes/missions/bangkok/tod_zika_night.brick",
+        "assembly:/_pro/scenes/missions/bangkok/tod_warlord_hot_midday.brick"
+    ],
+    "aab7a569-cead-40b8-81b5-c1605070b366": [
+        "assembly:/_pro/scenes/missions/colombia/mission_hot_anaconda.brick",
+        "assembly:/_pro/scenes/missions/colombia/gamechanger_hippo_zinnia.brick",
+        "assembly:/_pro/scenes/missions/colombia/gamechanger_hippo_holly.brick"
+    ],
+    "4a95baa5-6ad6-43c8-ae3e-be8449d578ec": [
+        "assembly:/_pro/scenes/missions/colorado_2/scenario_rabies_spades.brick",
+        "assembly:/_pro/scenes/missions/colorado_2/gamechanger_wild_card_daisy.brick",
+        "assembly:/_pro/scenes/missions/colorado_2/lighting_day.brick"
+    ],
+    "47d10233-0008-4f34-abd2-a84ca2bb7530": [
+        "assembly:/_pro/scenes/missions/coastaltown/tod_ets_sunset.brick",
+        "assembly:/_pro/scenes/missions/coastaltown/gamechanger_wild card_jasmine.brick",
+        "assembly:/_pro/scenes/missions/coastaltown/tod_ebola_night.brick"
+    ],
+    "dec0ed42-41bf-4ff0-bc0a-08d73b3e7430": [
+        "assembly:/_pro/scenes/missions/ancestral/tod_midday.brick",
+        "assembly:/_pro/scenes/missions/ancestral/override_bridge.brick"
+    ],
+    "c147e93f-2fa6-4618-a84e-d929c5d3d391": [
+        "assembly:/_pro/scenes/missions/edgy/mission_fox/tod_et_midday.brick",
+        "assembly:/_pro/scenes/missions/edgy/mission_fox/tod_sunset.brick",
+        "assembly:/_pro/scenes/missions/edgy/mission_fox/override_meyer.brick"
+    ],
+    "d2419fe4-ea72-4e61-b91b-bb39706f551d": [
+        "assembly:/_pro/scenes/missions/opulent/mission_stingray/tod_night.brick",
+        "assembly:/_pro/scenes/missions/opulent/mission_stingray/override_hilo.brick"
+    ],
+    "0d2a243a-f140-4be5-bfe7-e151878ec88e": [
+        "assembly:/_pro/scenes/missions/mumbai/tod_night.brick",
+        "assembly:/_pro/scenes/missions/mumbai/tod_midday.brick",
+        "assembly:/_pro/scenes/missions/mumbai/gamechanger_monkshood_holyman_override.brick"
+    ],
+    "5162c4a1-4b07-43b1-82ad-37c9e350764c": [
+        "assembly:/_pro/scenes/missions/wet/tod_redsnapper_cloudy.brick",
+        "assembly:/_pro/scenes/missions/wet/tod_midday.brick",
+        "assembly:/_pro/scenes/missions/wet/mission_mild_mahjong.brick"
+    ],
+    "6efd360a-5f78-44ac-9823-df077018c2b3": [
+        "assembly:/_pro/scenes/missions/skunk/tod_elusive_night.brick",
+        "assembly:/_pro/scenes/missions/skunk/override_solitaire.brick"
+    ],
+    "ee67660e-4bd6-4743-9163-40331056f690": [
+        "assembly:/_pro/scenes/missions/miami/tod_night_ass.brick",
+        "assembly:/_pro/scenes/missions/miami/override_pinochle.brick"
+    ],
+    "aa6447f6-7566-4e54-b1c9-15fa3d4acb39": [
+        "assembly:/_pro/scenes/missions/theark/tod_day_ets.brick",
+        "assembly:/_pro/scenes/missions/theark/gamechanger_magpie_locked_jail_door.brick"
+    ]
+}
